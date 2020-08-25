f7795dc14e3c39c84770fb560b59e1e5c95890cb
민파파 <florakid@naver.com>
2020-08-25 17:02:52 +0900

    Updates

---
+++ precache-manifest.972432c18b1ee0c7d070d02e11aa9f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbf8c076d19f222b33556af95efb9d4",
+    "url": "/movie_app_2021/index.html"
+  },
+  {
+    "revision": "e89c9074da6fa2d16125",
+    "url": "/movie_app_2021/static/css/main.a6743ea5.chunk.css"
+  },
+  {
+    "revision": "e42b8aa8ac443f72bf3b",
+    "url": "/movie_app_2021/static/js/2.c95c2308.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2021/static/js/2.c95c2308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89c9074da6fa2d16125",
+    "url": "/movie_app_2021/static/js/main.09495969.chunk.js"
+  },
+  {
+    "revision": "4c9b4f0544d13b330fc4",
+    "url": "/movie_app_2021/static/js/runtime-main.ddc9a99e.js"
+  }
+]);
